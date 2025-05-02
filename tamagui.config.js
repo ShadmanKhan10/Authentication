12a2aac6712e1af459d4948d0b7bd69223ce9171
SHADMAN KHAN <khanshadman7777@gmail.com>
2025-05-02 11:50:21 +0530

    Added some basic themes

--- tamagui.config.js
+++ tamagui.config.js
@@ -3,17 +3,26 @@ import { config } from "@tamagui/config";
 
 const tamaguiConfig = createTamagui({
   ...config,
-  // Define your custom themes here
   theme: {
     light: {
       background: "#FFFFFF",
       gray7: "#B0B0B0",
-      // Customize other colors if necessary
+      primary: "#4f46e5",
+      color: "#000000",
     },
     dark: {
       background: "#333333",
       gray7: "#888888",
-      // Customize other colors for dark theme
+      background: "#1a1a1a",
+      gray7: "#888888",
+      primary: "#22d3ee",
+      color: "#ffffff",
+    },
+    blue: {
+      background: "#e0f2ff",
+      gray7: "#60a5fa",
+      primary: "#3b82f6",
+      color: "#1e3a8a",
     },
   },
 });
